cf15dfc6fd3e05f0a4020eb6989ad3a8d3cfbb2d
Diego Abarca <di.abarcag@alumnos.duoc.cl>
2020-11-11 01:05:40 -0300

    Commit cambio de index a login, redireccion de paginas a index

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('index',views.home,name='home'),
     path('contenido/acer', views.acer, name='acer'),
     path('contenido/lenovo', views.lenovo, name='lenovo'),
     path('contenido/asus', views.asus, name='asus'),
-    path('login', views.login, name='login'),
+    path('', views.login, name='login'),
     path('registro', views.registro, name='registro'),
 ]
