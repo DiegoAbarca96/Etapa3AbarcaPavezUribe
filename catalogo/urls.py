f1dd2446a2b31af62fb21cc4d23e790747ec64cd
Diego Abarca <di.abarcag@alumnos.duoc.cl>
2020-11-11 23:57:51 -0300

    Commit arreglos graficos

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index',views.home,name='home'),
+    path('',views.home,name='home'),
     path('contenido/acer', views.acer, name='acer'),
     path('contenido/lenovo', views.lenovo, name='lenovo'),
     path('contenido/asus', views.asus, name='asus'),
-    path('', views.login, name='login'),
+    path('login', views.login, name='login'),
     path('registro', views.registro, name='registro'),
     path('producto', views.producto, name='producto'),
 ]
