d44992ecddc255be44b9d8f9f064f0ec13dc71e3
Diego Abarca <di.abarcag@alumnos.duoc.cl>
2020-12-10 19:39:16 -0300

    2

--- catalogo/test/test_forms.py
+++ catalogo/test/test_forms.py
@@ -1,31 +1,31 @@
-# from django.test import TestCase
-# from .forms import ProductoForm
+#from django.test import TestCase
+#from catalogo.forms import ProductoForm
 
 
-class TestForms(TestCase):
+#class TestForms(TestCase):
 
-   # def test_producto_form_valid_data(self):
-        #form = ProductoForm(data={
-       #     'nombre': 'asus',
+ #   def test_producto_form_valid_data(self):
+  ##      form = ProductoForm(data={
+    #        'nombre': 'asus',
      #       'categoria': 'notebook',
-     #       'marca': 'asus'
-     #   })
+      #      'marca': 'asus'
+       # })
 
-       # self.assertTrue(form.is_valid())
+        #self.assertTrue(form.is_valid())
     
-   # def test_producto_nombre_valid(self):
-        #form = ProductoForm(data={'nombre': 'asus'})
-       # self.assertTrue(form.is_valid())
+#    def test_producto_nombre_valid(self):
+ ##       form = ProductoForm(data={'nombre': 'asus'})
+   #     self.assertTrue(form.is_valid())
     
    # def test_producto_marca_no_valid(self):
-       # form = ProductoForm(data={'marca': '123$$`+'})
-       # self.assertFalse(form.is_valid())
+    ##    form = ProductoForm(data={'marca': '123$$`+'})
+      #  self.assertFalse(form.is_valid())
 
-   # def test_producto_categoria_no_valid(self):
-       # form = ProductoForm(data={'categoria': 'telefono'})
-       # self.assertFalse(form.is_valid())
+    #def test_producto_categoria_no_valid(self):
+     #   form = ProductoForm(data={'categoria': 'telefono'})
+      #  self.assertFalse(form.is_valid())
 
-   # def test_producto_no_data(self):
+    #def test_producto_no_data(self):
        # form = ProductoForm(data={})
-       # self.assertFalse(form.is_valid())
-       # self.assertEquals(len(form.errors), 8)
+     #   self.assertFalse(form.is_valid())
+      #  self.assertEquals(len(form.errors), 8)
