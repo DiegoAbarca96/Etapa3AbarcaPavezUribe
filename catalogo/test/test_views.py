77c8f859c192057c7c298a2a536e05a425933472
Diego Abarca <di.abarcag@alumnos.duoc.cl>
2020-12-10 20:56:14 -0300

    Entrega Etapa 3

---
+++ catalogo/test/test_views.py
@@ -0,0 +1,14 @@
+#from django.test import TestCase
+#from catalogo.models import Producto
+#from django.contrib.auth.models import User
+
+
+#class LoanedBookInstancesByUserListViewTest(TestCase):
+
+    #def setUp(self):
+        # Create two users
+        #test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        #test_user2 = User.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
+
+        #test_user1.save()
+        #test_user2.save()
